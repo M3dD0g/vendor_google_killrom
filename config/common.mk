25544129d7381a5d2d9c09b696ff1a810917892b
dhacker29 <davidhackerdvm@gmail.com>
2012-12-25 21:23:14 -0600

    Fix keyboard

--- config/common.mk
+++ config/common.mk
@@ -4,7 +4,6 @@
 PRODUCT_COPY_FILES += \
     vendor/google/killrom/system/app/CalendarProvider.apk:$(PRODUCT_OUT)/system/app/CalendarProvider.apk \
     vendor/google/killrom/system/app/OccamQuickOffice.apk:$(PRODUCT_OUT)/system/app/OccamQuickOffice.apk \
-    vendor/google/killrom/system/app/LatinIME.apk:$(PRODUCT_OUT)/system/app/LatinIME.apk \
     vendor/google/killrom/system/app/StreetView.apk:$(PRODUCT_OUT)/system/app/StreetView.apk \
     vendor/google/killrom/system/app/Maps.apk:$(PRODUCT_OUT)/system/app/Maps.apk \
     vendor/google/killrom/system/app/Calendar.apk:$(PRODUCT_OUT)/system/app/Calendar.apk \
@@ -37,7 +36,10 @@ PRODUCT_COPY_FILES += \
 
 ifeq ($(TARGET_PRODUCT),full_mako)
 PRODUCT_COPY_FILES += \
-    vendor/google/killrom/system/app/mako/GalleryGoogle.apk:$(PRODUCT_OUT)/system/app/GalleryGoogle.apk
+    vendor/google/killrom/system/app/mako/GalleryGoogle.apk:$(PRODUCT_OUT)/system/app/GalleryGoogle.apk \
+    vendor/google/killrom/system/app/mako/LatinIME.apk:$(PRODUCT_OUT)/system/app/LatinIME.apk
+else
+    vendor/google/killrom/system/app/LatinIME.apk:$(PRODUCT_OUT)/system/app/LatinIME.apk \
 endif
 
 ifneq ($(filter full_maguro full_toro,$(TARGET_PRODUCT)),)
